a3494e483c4777c12f7f5f23d53b05a09108e968
Piotr Dudziński <pdudzinski@naviexpert.pl>
2020-04-30 19:00:04 +0200

    Initial commit of clinic app

---
+++ src/main/kotlin/pl/dudzinski/clinic/ClinicApplication.kt
@@ -0,0 +1,11 @@
+package pl.dudzinski.clinic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClinicApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClinicApplication>(*args)
+}
---
+++ src/test/kotlin/pl/dudzinski/clinic/ClinicApplicationTests.kt
@@ -0,0 +1,14 @@
+package pl.dudzinski.clinic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClinicApplicationTests {
+
+	@Test
+	fun contextLoads() {
+
+	}
+
+}
